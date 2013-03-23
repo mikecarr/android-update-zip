b9dfc05d4da8e134b3f130910f378b0681f9ccf6
Mike Carr <mcarr@qualcomm.com>
2013-03-22 21:58:09 -0700

    move zip to out folder

---
+++ README.md
@@ -0,0 +1,8 @@
+## Set of tools used to create android update files
+
+create-zip.sh     :  zips up data and META-INF folders to the out folder
+resync.sh         :  cleans folders and pulls fresh copy from git
+
+
+
+I am a total rookie at this, I have to start somewhere.
--- create-zip.sh
+++ create-zip.sh
@@ -3,8 +3,13 @@
 FILE_NAME=android-update
 ZIP_NAME=$FILE_NAME.zip
 BACKUP_FOLDER=backup/
+OUT_ZIP_FOLDER=out/
 
-if [ -f $ZIP_NAME ];
+if [ ! -d $OUT_ZIP_FOLDER ]; then
+   mkdir $OUT_ZIP_FOLDER
+fi
+
+if [ -f $OUT_ZIP_FOLDER/$ZIP_NAME ];
 then
    echo "File $ZIP_NAME exists, moving to backup folder."
    if [ ! -d $BACKUP_FOLDER ];
@@ -12,8 +17,8 @@ then
      mkdir $BACKUP_FOLDER
    fi
    NOW=$(date +"%m-%d-%Y")-$(date +"%T")
-   mv $ZIP_NAME $BACKUP_FOLDER/$FILE_NAME-$NOW.zip
+   mv $OUT_ZIP_FOLDER/$ZIP_NAME $BACKUP_FOLDER/$FILE_NAME-$NOW.zip
 fi
 
 find . -name ".DS_Store" -exec rm -rf {} \;
-zip -r $ZIP_NAME data META-INF
+zip -r $OUT_ZIP_FOLDER/$ZIP_NAME data META-INF
